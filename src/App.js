7da2727f4854d3d83244b9445df5584ad16d7ccd
Alex Holyk <alexholyk@Alexs-MacBook-Pro.local>
2020-11-20 14:20:42 +0200

    fix sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CKEditor from "ckeditor4-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TwoWayBinding extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.onEditorChange = this.onEditorChange.bind(this);
+    this.onPublishClick = this.onPublishClick.bind(this);
+  }
+
+  onEditorChange(evt) {
+    this.setState({
+      data: evt.editor.getData(),
+    });
+  }
+
+  handleChange(changeEvent) {
+    this.setState({
+      data: changeEvent.target.value,
+    });
+  }
+
+  onPublishClick() {
+    const data = new Blob([this.state.data], { type: "text/html" });
+    const htmlURL = window.URL.createObjectURL(data);
+    let tempLink = document.createElement("a");
+    tempLink.href = htmlURL;
+    tempLink.setAttribute("download", "filename.html");
+    tempLink.click();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Editor</h1>
+        <CKEditor data={this.state.data} onChange={this.onEditorChange} />
+        <br />
+        <button onClick={this.onPublishClick} type="button">
+          Publish
+        </button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TwoWayBinding;
